c7d53016d80e56dc0b4313e74cf813c0cd4854c0
Tim Menzel <tim_menzel@t-online.de>
2023-03-11 18:32:59 +0100

    Init Jetpack, added first template activity

--- app/src/main/java/com/github/mrpaulblack/personalrecipes/MainActivity.kt
+++ app/src/main/java/com/github/mrpaulblack/personalrecipes/MainActivity.kt
@@ -2,10 +2,45 @@ package com.github.mrpaulblack.personalrecipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "Hello mom")
+                Spacer(modifier = Modifier.padding(top=16.dp))
+                Button(onClick = {}) {
+                    Text(text = "This is a button")
+                }
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
