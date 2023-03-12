b5d799413226e6c18918e70f08d53c1edf01f1bb
gkarl <georg.karl@stud.htwk-leipzig.de>
2023-03-12 12:47:43 +0100

    init lazycolumn

--- app/src/main/java/com/github/mrpaulblack/personalrecipes/MainActivity.kt
+++ app/src/main/java/com/github/mrpaulblack/personalrecipes/MainActivity.kt
@@ -1,17 +1,67 @@
 package com.github.mrpaulblack.personalrecipes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            personalrecipesTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Android", "Jetpack Compose"))
+                }
+            }
 
-        supportFragmentManager.beginTransaction().replace(R.id.container, CounterFragment()).commit();
-    }
-
-    override fun onBackPressed() {
-        supportFragmentManager.popBackStack();
+        }
     }
 }
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Column {
+        Image(
+            painter = painterResource(R.drawable.testpic),
+            contentDescription = "test picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(150.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = msg.author)
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = msg.body)
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    ComposeTutorialTheme {
+        Surface {
+            MessageCard(
+                msg = Message("Colleague", "Take a look at Jetpack Compose, it's great!")
+            )
+        }
+    }
+}
+
+
