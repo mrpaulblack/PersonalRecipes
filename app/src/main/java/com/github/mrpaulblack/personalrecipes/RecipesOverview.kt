a3aa16ef291aadbfc0917b07c22376d54c46ccf5
gkarl <georg.karl@stud.htwk-leipzig.de>
2023-03-12 18:53:29 +0100

    card hight adjustment

--- app/src/main/java/com/github/mrpaulblack/personalrecipes/RecipesOverview.kt
+++ app/src/main/java/com/github/mrpaulblack/personalrecipes/RecipesOverview.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,7 +40,7 @@ fun RecipesGrid(amount: Int) {
 fun RecipeCard(rec: Recipe) {
     ElevatedCard(
         onClick = { /* Do something */ },
-        modifier = Modifier.size(width = 180.dp, height = 240.dp)
+        modifier = Modifier.size(width = 180.dp, height = 154.dp)
 
     ) {
         Box(Modifier.fillMaxSize()) {
@@ -47,8 +48,10 @@ fun RecipeCard(rec: Recipe) {
                 Image(
                     painter = painterResource(R.drawable.testpic),
                     contentDescription = "test picture",
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
-                        .size(180.dp)
+                        .width(180.dp)
+                        .height(100.dp)
                 )
                 Row() {
                     Spacer(modifier = Modifier.width(16.dp))
