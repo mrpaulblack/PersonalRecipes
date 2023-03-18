bccee571fe0a5bcd185a2395f0bc6cb8fe6797ba
mrpaulblack <paul@paulgo.io>
2023-03-18 09:36:18 +0100

    [WIP] fix MainActivity; remove unused code and add dep

--- app/src/main/java/com/github/mrpaulblack/personalrecipes/MainActivity.kt
+++ app/src/main/java/com/github/mrpaulblack/personalrecipes/MainActivity.kt
@@ -2,6 +2,7 @@ package com.github.mrpaulblack.personalrecipes
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Scaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -44,71 +45,3 @@ class MainActivity : AppCompatActivity() {
 // this prevents opening the same route multiple times on the stack when for example the same tab is pressed on an always visible nav bar
 fun NavHostController.navigateSingleTop(route: String) =
     this.navigate(route) { launchSingleTop = true }
-
-data class Message(val author: String, val body: String)
-data class Recipe(val title: String, val text: String)
-
-@Composable
-fun RecipesGrid(amount: Int) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        horizontalArrangement = Arrangement.spacedBy(12.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp),
-        modifier = Modifier
-            .padding(
-                start = 12.dp,
-                end = 12.dp,
-                top = 12.dp,
-                bottom = 12.dp
-            )
-    ) {
-        items(amount) { i ->
-            RecipeCard(Recipe("YumYum", "Schmackofatz"))
-            Spacer(modifier = Modifier.height(4.dp))
-        }
-    }
-}
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun RecipeCard(rec: Recipe) {
-    ElevatedCard(
-        onClick = { /* Do something */ },
-        modifier = Modifier.size(width = 180.dp, height = 250.dp)
-
-    ) {
-        Box(Modifier.fillMaxSize()) {
-            Column {
-                Image(
-                    painter = painterResource(R.drawable.testpic),
-                    contentDescription = "test picture",
-                    modifier = Modifier
-                        .size(180.dp)
-                        .clip(CircleShape)
-
-                )
-                Row() {
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Column() {
-                        Spacer(modifier = Modifier.height(4.dp))
-                        Text(text = rec.title)
-                        Spacer(modifier = Modifier.height(2.dp))
-                        Text(text = rec.text)
-                    }
-
-                }
-
-            }
-        }
-    }
-}
-
-@Preview
-@Composable
-fun PreviewConversation() {
-        RecipesGrid(10)
-}
-
-
-
