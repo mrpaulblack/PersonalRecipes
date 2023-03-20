e9c2c85e83590c17f2075026bbc497e47d2f46b6
mrpaulblack <paul@paulgo.io>
2023-03-20 01:01:45 +0100

    feat: adding some paddings to search view

--- app/src/main/java/com/github/mrpaulblack/personalrecipes/ui/search/SearchView.kt
+++ app/src/main/java/com/github/mrpaulblack/personalrecipes/ui/search/SearchView.kt
@@ -11,8 +11,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import com.github.mrpaulblack.personalrecipes.R
 import com.github.mrpaulblack.personalrecipes.data.models.RecipeModel
 import com.github.mrpaulblack.personalrecipes.ui.components.RecipeCard
 import org.koin.androidx.compose.koinViewModel
@@ -36,26 +38,29 @@ object SearchView {
         Surface(
             modifier = modifier.fillMaxSize()
         ) {
-            Column(modifier = Modifier.fillMaxSize()) {
-                Row() {
-                    OutlinedTextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        value = textState.value,
-                        label = { Text("Search") },
-                        singleLine = true,
-                        onValueChange = {
-                            textState.value = it
-                            viewModel.query(textState)
-                        }
-                    )
-                }
+            Column(
+                modifier = Modifier.padding(horizontal = 12.dp)
+            ) {
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = textState.value,
+                    label = { Text(stringResource(R.string.nav_search)) },
+                    singleLine = true,
+                    onValueChange = {
+                        textState.value = it
+                        viewModel.query(textState)
+                    }
+                )
+                Spacer(modifier = Modifier.height(12.dp))
                 LazyVerticalGrid(
                     columns = GridCells.Fixed(2),
                     horizontalArrangement = Arrangement.spacedBy(12.dp),
                     verticalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
-                    items (recipesList.size) {
-                        RecipeCard.Content(recipesList[it], onClick)
+                    if (textState.value.text != "") {
+                        items (recipesList.size) {
+                            RecipeCard.Content(recipesList[it], onClick)
+                        }
                     }
                 }
             }
